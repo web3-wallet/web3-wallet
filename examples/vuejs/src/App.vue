a80c74f1c6b454c0e5a291b131155fd21651f780
logan <logan.luo@crypto.com>
2022-07-10 21:48:23 +0800

    add cjs build

--- examples/vuejs/src/App.vue
+++ examples/vuejs/src/App.vue
@@ -3,10 +3,12 @@
 </template>
 
 <script setup lang="ts">
+// import { MAX_SAFE_CHAIN_ID } from '@web3-wallet/hello-world';
 import { defineComponent } from 'vue';
 
 import MetaMask from './components/MetaMask.vue';
 
+// console.log(MAX_SAFE_CHAIN_ID);
 defineComponent({
   name: 'App',
   components: {
