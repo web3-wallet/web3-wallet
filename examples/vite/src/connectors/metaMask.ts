14c2741ecb3192083c0dd2b9113d3fad9c71f459
logan <logan.luo@crypto.com>
2022-07-10 17:53:58 +0800

    remove vuejs

--- examples/vite/src/connectors/metaMask.ts
+++
@@ -1,6 +0,0 @@
-import { MetaMask } from '@vvswallet/metamask';
-import { createWallet } from '@vvswallet/vue';
-
-export const metaMask = createWallet<MetaMask>(
-  (actions) => new MetaMask({ actions }),
-);
