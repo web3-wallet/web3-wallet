056014b79ea1ef422b1981788aa97b1b4047ba84
logan <logan.luo@crypto.com>
2022-07-13 17:52:11 +0800

    add trust wallet connector

---
+++ connectors/trust-wallet/src/index.ts
@@ -0,0 +1,23 @@
+import {
+  type MetaMaskCompatibleConstructorArgs,
+  type MetaMaskCompatibleProvider,
+  MetaMaskCompatible,
+} from '@web3-wallet/metamask-compatible';
+
+export type TrustWalletConstructorArgs = MetaMaskCompatibleConstructorArgs;
+
+export type TrustWalletProvider = MetaMaskCompatibleProvider & {
+  isTrust?: boolean;
+};
+
+const providerFilter = (p: TrustWalletProvider) => !!p.isTrust;
+
+export class TrustWallet extends MetaMaskCompatible {
+  public override detectProvider = async () => {
+    return await super.detectProvider(providerFilter);
+  };
+
+  constructor(arg: TrustWalletConstructorArgs) {
+    super(arg);
+  }
+}
