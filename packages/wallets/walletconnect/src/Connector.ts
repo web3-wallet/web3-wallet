8c367b219129f5e9991c9036b57a62291ee378d0
Logan Luo <logan.luo@crypto.com>
2022-09-03 11:37:04 +0800

    refactor: walletconnect

--- packages/wallets/walletconnect/src/Connector.ts
+++ packages/wallets/walletconnect/src/Connector.ts
@@ -5,11 +5,8 @@ import {
   type Provider,
   type WalletName,
   Connector,
-  utils,
 } from '@web3-wallet/core';
 import EventEmitter3 from 'eventemitter3';
-
-import { getBestUrl } from './utils';
 
 export const URI_AVAILABLE = 'URI_AVAILABLE';
 
@@ -17,52 +14,23 @@ type WalletConnectProvider = _WalletConnectProvider & Provider;
 
 export const walletName = 'WalletConnect' as WalletName<'WalletConnect'>;
 
-type WalletConnectOptions = Omit<
-  IWCEthRpcConnectionOptions,
-  'rpc' | 'infuraId' | 'chainId'
-> & {
-  rpc: { [chainId: number]: string | string[] };
-};
-
 export class WalletConnectConnector extends Connector {
   public provider?: WalletConnectProvider;
   public readonly events = new EventEmitter3();
 
-  private readonly options: Omit<WalletConnectOptions, 'rpc'>;
-  private readonly rpc: { [chainId: number]: string[] };
-  private readonly defaultChainId: number;
-  private readonly timeout: number;
+  private readonly options: IWCEthRpcConnectionOptions;
 
   constructor({
     actions,
     options,
     onError,
-    defaultChainId = 1,
-    timeout = 5000,
   }: {
     actions: Actions;
-    options: WalletConnectOptions;
+    options: IWCEthRpcConnectionOptions;
     onError?: Connector['onError'];
-    defaultChainId?: number;
-    timeout?: number;
   }) {
     super(walletName, actions, onError);
-
-    const { rpc, ...rest } = options;
-    this.options = rest;
-    this.rpc = Object.keys(rpc).reduce<{ [chainId: number]: string[] }>(
-      (acc, chainId) => {
-        const value = rpc[utils.parseChainId(chainId)];
-        acc[utils.parseChainId(chainId)] = Array.isArray(value)
-          ? value
-          : [value];
-        return acc;
-      },
-      {},
-    );
-    this.defaultChainId =
-      defaultChainId ?? utils.parseChainId(Object.keys(this.rpc)[0]);
-    this.timeout = timeout;
+    this.options = options;
   }
 
   private onDisplayUri = (
@@ -73,30 +41,10 @@ export class WalletConnectConnector extends Connector {
   };
 
   public async detectProvider(): Promise<WalletConnectProvider> {
-    // because we can only use 1 url per chainId, we need to decide between multiple, where necessary
-    const results = await Promise.all(
-      Object.keys(this.rpc).map(
-        async (chainId): Promise<[number, string]> => [
-          utils.parseChainId(chainId),
-          await getBestUrl(this.rpc[utils.parseChainId(chainId)], this.timeout),
-        ],
-      ),
-    );
-
-    const rpc = results.reduce<{ [chainId: number]: string }>(
-      (acc, [chainId, url]) => {
-        acc[chainId] = url;
-        return acc;
-      },
-      {},
-    );
-
     const m = await import('@walletconnect/ethereum-provider');
 
     const provider = new m.default({
       ...this.options,
-      chainId: this.defaultChainId,
-      rpc,
     }) as unknown as WalletConnectProvider;
 
     this.provider = provider;
