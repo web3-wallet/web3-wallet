0a160524078231ca19016e76e856028f57fa3076
Logan Luo <logan.luo@crypto.com>
2022-09-04 08:33:35 +0800

    feat: add global.d.ts

--- site/global.d.ts
+++ site/global.d.ts
@@ -1,5 +1,23 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly IS_LOCAL: 'true' | undefined;
+declare global {
+  const __CLIENT__: boolean;
+  const __SERVER__: boolean;
+  const __DEV__: boolean;
+  const __SITE_PREFIX__: string;
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly NODE_ENV: 'development' | 'production';
+      /**
+       * if you are trying to run the site in your local machine
+       * you should crate a `site/.env.local` file and set IS_LOCAL
+       * to true in `site/.env.local`.
+       */
+      readonly IS_LOCAL: 'true' | undefined;
+    }
   }
 }
+
+/**
+ * Don't remove the empty export
+ */
+export {};
