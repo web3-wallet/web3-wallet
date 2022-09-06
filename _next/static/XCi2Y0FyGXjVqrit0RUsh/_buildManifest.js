d58d6ca9f2d6af432dbf8a51a9a875508692300f
LoganAtCrypto <LoganAtCrypto@users.noreply.github.com>
2022-09-06 14:20:57 +0000

    deploy: 026ac61f03275d804e4e4f3d4fca0a7cf1e22210

---
+++ _next/static/XCi2Y0FyGXjVqrit0RUsh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/393-a7213920b654ad7b.js","static/chunks/pages/index-3e66a68c30be813f.js"],"/_error":["static/chunks/pages/_error-f718916d71b1548f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
