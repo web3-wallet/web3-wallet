a0401b39f64473739e78adc602f9f20928f8bf5b
LoganAtCrypto <LoganAtCrypto@users.noreply.github.com>
2022-09-06 13:52:11 +0000

    deploy: 96bb2fdda7420b5a2d87d38f8c9db5a6f9205338

--- _next/static/Cx4LUrY-nloot7f3GrScp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/393-a7213920b654ad7b.js","static/chunks/pages/index-16937fb0eaa6ee16.js"],"/_error":["static/chunks/pages/_error-f718916d71b1548f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
