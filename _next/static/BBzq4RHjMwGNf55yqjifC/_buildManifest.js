1e06e1a2c31e201fc2361c44222f12e4dae68bb9
LoganAtCrypto <LoganAtCrypto@users.noreply.github.com>
2022-09-06 11:25:37 +0000

    deploy: b147368aeed2560ab9a92afa80b5afdcebb44288

---
+++ _next/static/BBzq4RHjMwGNf55yqjifC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/393-a7213920b654ad7b.js","static/chunks/pages/index-838f9baa9f85f1c4.js"],"/_error":["static/chunks/pages/_error-f718916d71b1548f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/k9SaRSdT76WMX51GrtDto/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/393-a7213920b654ad7b.js","static/chunks/pages/index-07f8c2187d1eaed2.js"],"/_error":["static/chunks/pages/_error-f718916d71b1548f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
