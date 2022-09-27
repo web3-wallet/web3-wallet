9f0aeda1871409122f24e8e6172f5fb62eeb52ad
Logan Luo <logan.luo@crypto.com>
2022-09-27 21:14:29 +0800

    feat: improve Web3Provider syncronously

--- packages/react/src/plugins/web3-provider/plugin.ts
+++ packages/react/src/plugins/web3-provider/plugin.ts
@@ -1,12 +1,11 @@
-import type {
-  BaseProvider,
-  Networkish,
+import {
+  type BaseProvider,
+  type Networkish,
   Web3Provider,
 } from '@ethersproject/providers';
 import { useMemo } from 'react';
 
 import type { CreatePlugin, Plugin, PluginName } from '../../types';
-import { useImportWeb3Provider } from './useImportWeb3Provider';
 
 const _name = '@web3-wallet/plugin-web3-provider';
 export const name = _name as PluginName<typeof _name>;
@@ -21,27 +20,26 @@ export type Api = {
 
 export const create: CreatePlugin<undefined, Api> = () => {
   const createApi: Plugin<Api>['createApi'] = ({ wallet }) => {
-    const { useIsConnected, useChainId, $getProvider } = wallet;
+    const { useAccount, useChainId, $getProvider } = wallet;
 
     const useProvider = <T extends BaseProvider = Web3Provider>(
       network?: Networkish,
     ) => {
-      const isConnected = useIsConnected();
+      const account = useAccount();
       const chainId = useChainId();
-      const ImportedWeb3Provider = useImportWeb3Provider();
 
       return useMemo(() => {
         // to ensure connectors remain fresh, we condition re-renders
-        // when ImportedWeb3Provider isConnected, chainId changed
-        void isConnected && chainId;
+        // when ImportedWeb3Provider account, chainId changed
+        void account && chainId;
         const provider = $getProvider();
 
-        if (ImportedWeb3Provider && provider) {
-          return new ImportedWeb3Provider(provider, network) as unknown as T;
+        if (provider) {
+          return new Web3Provider(provider, network) as unknown as T;
         }
 
         return undefined;
-      }, [ImportedWeb3Provider, isConnected, chainId, network]);
+      }, [account, chainId, network]);
     };
 
     return {
