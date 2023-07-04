474b37a9c6083bd181ef828d821d0430d270d8e2
Logan Luo <logan.luo@cronoslabs.org>
2023-07-04 23:44:41 +0800

    feat: update examples/react

--- packages/examples/react/src/wallets/index.ts
+++ packages/examples/react/src/wallets/index.ts
@@ -45,7 +45,23 @@ export const walletConfigs: WalletConfig[] = [
     icon: WalletConnect.walletIcon,
     connector: new WalletConnect({
       providerOptions: {
-        rpc: rpcMap,
+        rpcMap,
+        /**
+         * @note Chains that your app intents to use and the peer MUST support.
+         *  If the peer does not support these chains, the connection will be rejected.
+         * @default [1]
+         * @example [1, 3, 4, 5, 42]
+         */
+        chains: [1],
+        /**
+         * @note Optional chains that your app MAY attempt to use and the peer MAY support.
+         *  If the peer does not support these chains, the connection will still be established.
+         * @default [1]
+         * @example [1, 3, 4, 5, 42]
+         */
+        optionalChains: [25],
+        showQrModal: true,
+        projectId: 'f30b7f87c783bab76df3a66876f2a67f',
       },
     }),
   },
